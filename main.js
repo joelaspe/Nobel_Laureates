7fc5fbb0132d8786ff7024b79852a0ee3e22ac68
joelaspe <joe.laspe@gmail.com>
2023-05-24 09:41:48 -0700

    Added category and year search

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
+/*** GLOBAL VARIABLES */
+const API_BASE_URL = 'https://api.nobelprize.org/2.1/nobelPrize/';
+
+/*** Event Listeners */
 const searchBtn = document.querySelector("#search-btn");
 searchBtn.addEventListener('click', getData);
 
+
+/** Makes the API call to the nobel prize API */
 async function getData() {
-    console.log('user clicked on the button');
-    const response = await fetch('https://api.nobelprize.org/2.1/nobelPrizes');
+    const yearInputElement = document.querySelector("#year-input");
+    const year = yearInputElement.value;
+    const apiString = generateApiStringCategoryAndYear('che', year);
+
+    const response = await fetch(apiString);
     const data = await response.json();
     console.log(data);   
 }
 
+function generateApiStringCategoryAndYear(category, year) {
+    return API_BASE_URL + category + "/" + year;
+}
